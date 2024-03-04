8fcbbf14b757f88ec343be2294b95b0dc82e068d
mahmoudellabban <mahmoud.ellabban.7@gmail.com>
2024-03-04 17:08:47 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css'; // You can create a separate CSS file for styling
+import './App.css'; 
 
 const RegisterPage = () => {
   const [activeCard, setActiveCard] = useState('signup');
@@ -14,12 +14,8 @@ const RegisterPage = () => {
       <div className="card">
         {activeCard === 'signup' ? (
           <>
-            {/* Sign Up card content */}
             <h2>Sign Up</h2>
             <form>
-              <label htmlFor="username">Username:</label>
-              <input type="text" id="username" name="username" />
-
               <label htmlFor="email">Email:</label>
               <input type="email" id="email" name="email" />
 
@@ -31,12 +27,10 @@ const RegisterPage = () => {
           </>
         ) : (
           <>
-            {/* Log In card content */}
             <h2>Log In</h2>
             <form>
-              <label htmlFor="loginUsername">Username:</label>
-              <input type="text" id="loginUsername" name="loginUsername" />
-
+            <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" />
               <label htmlFor="loginPassword">Password:</label>
               <input type="password" id="loginPassword" name="loginPassword" />
 
@@ -47,15 +41,20 @@ const RegisterPage = () => {
       </div>
       <div className="welcome-message">
         {activeCard === 'signup' ? (
-          <p>Welcome! Please sign up to continue.</p>
+          <div>
+          <p>Already have an account? Click to Log in</p>
+          <button onClick={() => handleCardToggle('login')}>Log In</button>
+          </div>
         ) : (
-          <p>Welcome back! Log in to access your account.</p>
+          
+          <div>
+          <p>Click to register if you don't have an account</p>
+          <button onClick={() => handleCardToggle('signup')}>Sign Up</button>
+          </div>
         )}
       </div>
     </div>
     <div className="card-toggle">
-        <button onClick={() => handleCardToggle('signup')}>Sign Up</button>
-        <button onClick={() => handleCardToggle('login')}>Log In</button>
       </div>
       </>
   );
